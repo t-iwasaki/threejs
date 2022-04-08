c6c51f68b2166133d56652562684a673aa41b552
t-iwasaki <takaiwa@gmail.com>
2022-04-08 17:03:22 +0900

    OrbitControlsを追加。x軸の回転を固定

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import * as THREE from "three";
 // オブジェクトをロードするための Loader をimportしておきます
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const controls = new OrbitControls(camera, document.body);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -34,13 +36,11 @@ camera.position.z = 6;
 const light = new THREE.AmbientLight(0xFFFFFF, 1.0);
 scene.add(light);
 
-
 const animate = function () {
   requestAnimationFrame( animate );
   // そのまま書くとオブジェクトが読み込まれる前に動いてしまうので、ifで括っておく
   if (object_keyboard){
     object_keyboard.rotation.x = 1;
-//    object_keyboard.rotation.x += 0.01;
     object_keyboard.rotation.y += 0.01;
   }
   renderer.render( scene, camera );
